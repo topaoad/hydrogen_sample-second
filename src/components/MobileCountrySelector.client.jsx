5cd9a45c33139aea9b5a14d57a60e0ac52be3d27
toproad <tattun0817@gmail.com>
2022-04-24 14:37:15 +0900

    www

--- src/components/MobileCountrySelector.client.jsx
+++ src/components/MobileCountrySelector.client.jsx
@@ -15,7 +15,7 @@ export default function MobileCountrySelector() {
   const [countries, setCountries] = useState([]);
 
   if (error) {
-    throw error;
+    throw error ;
   }
 
   useEffect(() => {
